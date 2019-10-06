5995f0ff729ac305b594f3f585bce23510c09752
Алексей <zimobox@gmail.com>
2019-10-07 02:02:59 +0400

    Initial commit

---
+++ app/src/main/java/ru/netology/kotlin/dz3_1standroidapp/MainActivity.kt
@@ -0,0 +1,61 @@
+package ru.netology.kotlin.dz3_1standroidapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+import ru.netology.kotlin.dz3_1standroidapp.adapter.PostAdapter
+import ru.netology.kotlin.dz3_1standroidapp.dto.Post
+import ru.netology.kotlin.dz3_1standroidapp.dto.PostType
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val meetup = Post(
+                4, "Netology", "«Цифровой прорыв» — всероссийский конкурс" +
+                 " для IT-специалистов, дизайнеров и управленцев в сфере цифровой экономики. " +
+                 "Участие бесплатное! Казань, 27-29 сентября 2019 года.", "15.08.2019",
+                true, false, true,
+                1, 5,3,
+                 PostType.EVENT,
+                "Казань",
+                55.798818 to 49.110315)
+
+        var list = mutableListOf(
+            Post(
+                8, "Netology", "First post in our network!", "20.08.2019",
+                false, true, true,
+                0, 8, 2,
+                 PostType.EVENT,
+                "Ижевск",
+                56.852562 to 53.208176),
+            Post(
+                7, "Netology", "First post in our network!", "19.08.2019",
+                true, false, true,
+                2, 13, 1,
+                 PostType.POST),
+            Post(6, "Netology", "Kotlin MeetUp!", "18.08.2019",
+                 likedByMe = false, postType = PostType.REPLY, source = meetup),
+            Post(5, "Netology", "Kotlin MeetUp!", "17.08.2019",
+                 likedByMe = false, postType = PostType.REPOST, source = meetup),
+            meetup,
+            Post(3, "Netology", "Реклама. Курсы по Котлину в Нетологии!",
+                "16.08.2019", true, false, true,
+                5, 4, 1,
+                 PostType.ADVERT,
+                 reference = "https://geekbrains.ru/professions/android_developer"),
+            Post(2, "Netology", "Посмотрите это видео о кошках!",
+                "15.08.2019", likedByMe = true, postType = PostType.VIDEO,
+                 reference = "https://www.youtube.com/watch?time_continue=1&v=BhJO2Urrq94"),
+            Post(1, "Netology", "First it in our network!", "14.08.2019")
+        )
+
+        with(container) {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = PostAdapter(list)
+        }
+    }
+}
---
+++ app/src/main/java/ru/netology/kotlin/dz3_1standroidapp/adapter/postfeed/AdvertViewHolder.kt
@@ -0,0 +1,44 @@
+package ru.netology.kotlin.dz3_1standroidapp.adapter.postfeed
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import ru.netology.kotlin.dz3_1standroidapp.adapter.PostAdapter
+import ru.netology.kotlin.dz3_1standroidapp.dto.Post
+//Отличие вьюхолдеров постов разных типов задается этой строчкой (ниже).
+//В каждом классе-вьюхолдере она ссылается на свою собственную форму вывода, поэтому
+//несмотря на то, что в разных формах есть одинаковые элементы с одинаковыми именами,
+//неопределенности не возникает, т.к. программа видит к какому конкретному элементу нужно
+//обратиться:
+import kotlinx.android.synthetic.main.post_feed_advert_card.view.*
+
+class AdvertViewHolder(adapter: PostAdapter, view: View)
+    : BaseViewHolder(adapter, view) {
+    init { //При инициализации вьюхолдера назначить универсальный лиснер кнопкам на форме
+        with (itemView) {
+            if (adapterPosition != RecyclerView.NO_POSITION) {
+                likeBtn.setOnClickListener(btnListener(adapter.list[adapterPosition], likeQtyTv))
+                commentBtn.setOnClickListener(btnListener(adapter.list[adapterPosition], commentQtyTv))
+                shareBtn.setOnClickListener(btnListener(adapter.list[adapterPosition], shareQtyTv))
+            }
+        }
+    }
+
+    //Абстрактный метод bind сделать реализованным, используя готовый общий для всех метод
+    // родительского класса, указывая в качестве параметров элементы текущей конкретной формы.
+    override fun bind(post: Post) {
+        with (itemView) {
+            super.bind(
+                post,
+                createdTv,
+                authorTv,
+                contentTv,
+                likeQtyTv,
+                commentQtyTv,
+                shareQtyTv,
+                likeBtn,
+                commentBtn,
+                shareBtn
+            )
+        }
+    }
+}
